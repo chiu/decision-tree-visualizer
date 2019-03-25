66b3011d3f11bce8b1712ba2a789469edfc5bb5c
vlc4 <vlc4@sfu.ca>
2019-03-25 11:36:08 -0700

    more changes to treant

--- treant-js/examples/decision-tree/basic-example.js
+++ treant-js/examples/decision-tree/basic-example.js
@@ -3,7 +3,8 @@ var config = {
         animateOnInit: true,
         node: {
             HTMLclass: "nodeExample1",
-            collapsable: true
+            collapsable: true,
+            collapsed: true
         },
         animation: {
             nodeAnimation: "easeOutBounce",
@@ -14,7 +15,7 @@ var config = {
     },
     node0 = { text: { gini: "gini:0.759", samples: "samples: 101", attribute: "attribute: milk <= 0.5", class_name: "class_name: Bug", value: "value: [4, 20, 8, 13, 10, 41, 5]" }, connectors: { type: "curve", style: { "stroke-width": 20.2 } }, image: "plots/gini0.png" },
     node1 = { parent: node0, text: { gini: "gini: 0.785", samples: "samples: 60", attribute: "attribute: feathers <= 0.5", class_name: "class_name: Bird", value: "value: [4, 20, 8, 13, 10, 0, 5]" }, connectors: { type: "curve", style: { "stroke-width": 12.0 } }, image: "plots/gini1.png" },
-    node2 = { parent: node1, text: { gini: "gini: 0.766", samples: "samples: 40", attribute: "attribute: fins <= 0.5", class_name: "class_name: Fish", value: "value: [4, 0, 8, 13, 10, 0, 5]" }, connectors: { type: "curve", style: { "stroke-width": 8.0 } }, image: "plots/gini2.png" },
+    node2 = { parent: node1, text: { gini: "gini: 0.766", samples: "samples: 40", attribute: "attribute: fins <= 0.5", class_name: "class_name: Fish", value: "value: [4, 0, 8, 13, 10, 0, 5]" }, connectors: { type: "curve", style: { "stroke-width": 8.0 } }, collapsable: false, image: "plots/gini2.png" },
     node3 = { parent: node2, text: { gini: "gini: 0.719", samples: "samples: 27", attribute: "attribute: backbone <= 0.5", class_name: "class_name: Amphibian", value: "value: [4, 0, 8, 0, 10, 0, 5]" }, connectors: { type: "curve", style: { "stroke-width": 5.4 } }, image: "plots/gini3.png" },
     node4 = { parent: node3, text: { gini: "gini: 0.494", samples: "samples: 18", attribute: "attribute: airborne <= 0.5", class_name: "class_name: Amphibian", value: "value: [0, 0, 8, 0, 10, 0, 0]" }, connectors: { type: "curve", style: { "stroke-width": 3.6 } }, image: "plots/gini4.png" },
     node5 = { parent: node4, text: { gini: "gini: 0.278", samples: "samples: 12", attribute: "attribute: predator <= 0.5", class_name: "class_name: Amphibian", value: "value: [0, 0, 2, 0, 10, 0, 0]" }, connectors: { type: "curve", style: { "stroke-width": 2.4 } }, image: "plots/gini5.png" },
