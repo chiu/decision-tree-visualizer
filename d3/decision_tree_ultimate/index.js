fef6b771e3d9ce33bfa93b7923b5cd0eaadd943b
vlc4 <vlc4@sfu.ca>
2019-04-18 00:25:32 -0700

    more improvements

--- d3/decision_tree_ultimate/index.js
+++ d3/decision_tree_ultimate/index.js
@@ -267,10 +267,10 @@ function click(d) {
             .attr("class", "attributetip")
             .attr("dx", 0)
             .attr("dy", 0)
-            .html("\<img src=\"https://s3-us-west-2.amazonaws.com/s.cdpn.io/3117222/v2" + d.name + ".png\" style=\"float:right;width:400px;height:600px;\">")
-        // .html("\<img src=\"python_plots2/v2" + d.name + ".png\" style=\"float:right;width:400px;height:600px;\">")
+            .html("\<img src=\"python_plots2/v2" + d.name + ".png\" style=\"float:right;width:400px;height:600px;\">")
 
-        // .html("\<img src=\"https://s3-us-west-2.amazonaws.com/s.cdpn.io/3117222/" + d.name + ".png\" style=\"float:right;width:400px;height:600px;\">")
+            // .html("\<img src=\"https://s3-us-west-2.amazonaws.com/s.cdpn.io/3117222/v2" + d.name + ".png\" style=\"float:right;width:400px;height:600px;\">")
+
 
     } else {
         d3.select("body").append("div")
